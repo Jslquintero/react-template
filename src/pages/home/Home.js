46dc6980d27d0fc806343a8c7a1d112329eee3b9
José López <jslquintero@gmail.com>
2022-12-27 15:53:14 -0500

    basic structure

---
+++ src/pages/home/Home.js
@@ -0,0 +1,19 @@
+/**
+   * @author: 
+   * @date: 
+   * @description: 
+   * @Editor: 
+   * @dateUpdate: 
+   * @descriptionUpdate: 
+   * @version: 1.0.0
+   * @param{}props
+ */
+
+import React from "react";
+
+
+export default function Home() {
+    return (<>
+        <h1>Home</h1>
+    </>);
+}
